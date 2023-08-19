d9993087d880419b0f442181860add313c2ec9ac
Dallin Hull <dallinrichard@gmail.com>
2023-08-19 17:51:21 -0600

    Commit complete Experience section with Vertical Timeline

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -39,11 +39,9 @@ const About = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p
-          className={styles.sectionSubText}
+        <p className={styles.sectionSubText}
         >Introduction</p>
-        <h2
-          className={styles.sectionHeadText}
+        <h2 className={styles.sectionHeadText}
         >Overview.</h2>
       </motion.div>
 
